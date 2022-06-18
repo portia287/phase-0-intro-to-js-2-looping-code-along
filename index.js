7f0ee7cc2e812fcbecbe1c0b007828c0a8467814
Portia Antwi <portiaantwi257@gmail.com>
2022-06-18 10:00:42 -0700

    loops

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+const events = "wedding";
+function writeCards(names,events){
+    let thankYourCards = []
+    for (let i = 0; i < names.length;i++)
+{
+    thankYourCards.push(`Thank you,& {names[i]}, for the wonderful & {events} gift!`)
+}
+return thankYourCards
+
+};
+function countDown(p){
+    let count = p 
+    while (count>=0){
+        console.log(count);
+        count -= 1
+    }
+
+
+}
+
+
