a0b2a86b3187de1d621549803ac102edb5e965d4
Portia Antwi <portiaantwi257@gmail.com>
2022-06-18 09:45:43 -0700

    commit tree 6c85adc7d4507cbed890a0911434129b40c35d5b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+   
+    const messages = [];
+    
+    for(let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        
+    }
+    return messages;
+}
+console.log(writeCards(["Guadlupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(){
+    for (let i = 0; i <=10; i++){
+        console.log(i)
+    }
+    
+
+ }
